4560c16a7f498e93ecd11a495031a699cad67da9
danijm26 <danijm26@gmail.com>
2025-09-19 22:40:28 +0200

    infra: Added infrastructure for running the service in aws. Fine tuned response headers for smooth streaming.

--- src/main/java/com/blockchain/blockpulseservice/controller/AnalysisController.java
+++ src/main/java/com/blockchain/blockpulseservice/controller/AnalysisController.java
@@ -2,6 +2,7 @@ package com.blockchain.blockpulseservice.controller;
 
 import com.blockchain.blockpulseservice.model.event.AnalyzedTransactionEvent;
 import com.blockchain.blockpulseservice.service.stream.AnalysisStream;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.codec.ServerSentEvent;
@@ -20,7 +21,9 @@ public class AnalysisController {
     private final AnalysisStream stream;
 
     @GetMapping(value = "/stream", produces = TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<AnalyzedTransactionEvent>> stream() {
+    public Flux<ServerSentEvent<AnalyzedTransactionEvent>> stream(HttpServletResponse response) {
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "keep-alive");
         return stream.flux()
                 .doOnSubscribe(s -> log.info("Stream subscribed."))
                 .doOnCancel(() -> log.info("Stream cancelled."))
